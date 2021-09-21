3c98c91a9c28b5ac138f57c22e2ceaad5cf7b50a
Emelie M <emelie.moya@aol.com>
2021-09-20 18:54:12 -0700

    made Profile that shows posts and messages of user

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,50 @@
 import React, { useState, useEffect } from 'react';
+import { getUserInfo } from '../api';
 
 const Profile = () => {
+    const [user, setUser] = useState({})
+
+    useEffect(async ()=>{
+      const userInfo = await getUserInfo()
+      setUser(userInfo.data)
+    }, [])
+
+    const userPosts = user.posts;
+    const userMessages = user.messages;
+    const userName = user.username;
+    console.log(user)
+
+    return <div><header className="profile-username">
+        <h2>Welcome {userName}!</h2>
+    </header>
+
+    <div className='profile-posts'>
+    <h2>Your Posts:</h2>
+    {userPosts.map((post)=>{
+        console.log(post)
+        return <div className="post-card">
+            <h2>{post.title}</h2>
+            <p>{post.description}</p>
+          </div>
+    })}
+
+    </div>
+
+    <div className="profile-messages">
+    <h2>Your Messages:</h2>
+    {userMessages.map((message)=>{
+      console.log(message)
+      return <div>
+        <h2>From User: {message.fromUser.username}</h2>
+        <h3>{message.post.title}</h3>
+        <p>{message.content}</p>
+      </div>
+    })}
+
+    </div>
+
+
+    </div>
 
 }
 
