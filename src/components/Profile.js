11c339c3c130e2628e242e1450d92a4f1e2edf58
Emelie M <emelie.moya@aol.com>
2021-09-20 19:41:40 -0700

    fixed Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,42 +5,40 @@ const Profile = () => {
     const [user, setUser] = useState({})
 
     useEffect(async ()=>{
-      const userInfo = await getUserInfo()
+      const userInfo = await getUserInfo();
       setUser(userInfo.data)
     }, [])
 
+    console.log(user)
     const userPosts = user.posts;
+    console.log(userPosts)
     const userMessages = user.messages;
     const userName = user.username;
-    console.log(user)
 
-    return <div><header className="profile-username">
+    return <div className="profile-container">
+    <header className="profile-username">
         <h2>Welcome {userName}!</h2>
     </header>
 
     <div className='profile-posts'>
     <h2>Your Posts:</h2>
-    {userPosts.map((post)=>{
-        console.log(post)
-        return <div className="post-card">
+    {userPosts && userPosts.length ? userPosts.map((post)=>{
+        return <div key={post._id} className="post-card">
             <h2>{post.title}</h2>
             <p>{post.description}</p>
           </div>
-    })}
-
+    }) : null}
     </div>
 
     <div className="profile-messages">
     <h2>Your Messages:</h2>
-    {userMessages.map((message)=>{
-      console.log(message)
-      return <div>
+    {userMessages && userMessages.length ? userMessages.map((message)=>{
+      return <div key={message._id}>
         <h2>From User: {message.fromUser.username}</h2>
         <h3>{message.post.title}</h3>
         <p>{message.content}</p>
       </div>
-    })}
-
+    }) : null}
     </div>
 
 
