c6dc9fe582e05e6be6f4654be897cc95d9013849
Emelie M <emelie.moya@aol.com>
2021-09-25 10:20:26 -0700

    fixed logout

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,13 +1,13 @@
-export function storeUser(token){
-    localStorage.setItem('token', JSON.stringify(token));
+export function storeUser(token) {
+  localStorage.setItem("token", JSON.stringify(token));
 }
 
-export function getToken(){
-    const myToken = JSON.parse(localStorage.getItem('token'));
-    return myToken
+export function getToken() {
+  const myToken = JSON.parse(localStorage.getItem("token"));
+  return myToken;
 }
 
-export function clearCurrentUser(){
-    localStorage.removeItem('currentUser')
-    setIsLoggedIn(false)
+export function clearCurrentUser() {
+  localStorage.removeItem("token");
+  setIsLoggedIn(false);
 }
